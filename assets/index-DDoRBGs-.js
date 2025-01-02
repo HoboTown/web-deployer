daf5ab9b936964ede5ed7af1903e2c448f4caf55
HoboTown <hobotownwtf@yahoo.com>
2025-01-01 21:40:13 -0700

    Add static.json to build

--- assets/index-DDoRBGs-.js
+++ assets/index-DDoRBGs-.js
@@ -1,4 +1,4 @@
-import{T as Pe,M as rt,R as k,E as z,C as O,a as ne,b as uo,O as K,c as me}from"./index-BvHckZxW.js";import{r as ho,l as mo}from"./index-DCrdQOvJ.js";/**
+import{T as Pe,M as rt,R as k,E as z,C as O,a as ne,b as uo,O as K,c as me}from"./index-BUo4d-OD.js";import{r as ho,l as mo}from"./index-Duq2NMRe.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
